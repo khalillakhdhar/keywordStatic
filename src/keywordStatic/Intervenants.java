467ec4dd979eb98e169fae22c45349191407a354
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-09-03 11:35:34 +0100

    code

--- src/keywordStatic/Intervenants.java
+++ src/keywordStatic/Intervenants.java
@@ -7,8 +7,11 @@ public class Intervenants {
 public static List<Intervention> interventions=new ArrayList<Intervention>();
 public  static void showList()
 {
+	System.out.println("le premier intervenant est "+interventions.get(0));
+	System.out.println("le dernier intervenant est "+interventions.get(interventions.size()-1));
 for(Intervention inter: interventions)
 {
+	
 System.out.println(inter.toString());	
 }
 }
